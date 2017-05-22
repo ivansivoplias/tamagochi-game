9670bd8aa50d98014ba3ca2ed5a6799b774d8a19
ivansyvoplias <ivan.kozaks@gmail.com>
2017-05-22 14:06:53 +0300

    Add simple indicator to game mode menu

--- Tamagochi/Views/MainWindow.xaml.cs
+++ Tamagochi/Views/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using Tamagochi.UI.ViewModels;
 
 namespace Tamagochi.UI.Views
@@ -13,6 +17,10 @@ namespace Tamagochi.UI.Views
         private Point _windowStartPosition;
         private Size _startSize;
         private WindowState _startState;
+        private Image _activeIcon;
+        //private Brush _highlightedBackground;
+        //private Brush _startBackground;
+        //private Brush _startBorderBrush;
 
         public MainWindow(GameViewModel viewModel)
         {
@@ -28,6 +36,18 @@ namespace Tamagochi.UI.Views
 
             _isInFullScreenMode = false;
             _windowStartPosition = new Point(0, 0);
+            //windowMode.IsChecked = true;
+
+            _activeIcon = new Image();
+            _activeIcon.Source = new BitmapImage(new Uri("pack://application:,,,/Images/Icons/active.png"));
+            _activeIcon.Width = 15;
+            _activeIcon.Height = 15;
+
+            //_highlightedBackground = new SolidColorBrush(Color.FromArgb(191, 147, 197, 255));
+            //_startBackground = windowMode.Background;
+            //_startBorderBrush = windowMode.BorderBrush;
+            //windowMode.Background = _highlightedBackground;
+            //windowMode.BorderBrush = _highlightedBackground;
         }
 
         private void FinishGameHandler(FinishGameViewModel model)
@@ -41,6 +61,18 @@ namespace Tamagochi.UI.Views
         {
             if (!_isInFullScreenMode && sender == fullScreenMode)
             {
+                //fullScreenMode.IsChecked = true;
+                windowMode.Icon = null;
+                fullScreenMode.Icon = _activeIcon;
+                //windowMode.IsChecked = false;
+                //windowMode.Background = _startBackground;
+                //windowMode.BorderBrush = _startBorderBrush;
+
+                //_startBackground = fullScreenMode.Background;
+
+                //fullScreenMode.Background = _highlightedBackground;
+                //fullScreenMode.BorderBrush = _highlightedBackground;
+
                 _startSize = new Size(ActualWidth, ActualHeight);
                 _windowStartPosition = new Point(Left, Top);
                 _startState = WindowState;
@@ -58,6 +90,19 @@ namespace Tamagochi.UI.Views
             }
             else if (sender == windowMode && _isInFullScreenMode)
             {
+                fullScreenMode.Icon = null;
+                windowMode.Icon = _activeIcon;
+                //windowMode.IsChecked = true;
+                //fullScreenMode.IsChecked = false;
+                //fullScreenMode.Background = _startBackground;
+                //fullScreenMode.BorderBrush = _startBorderBrush;
+
+                //_startBackground = windowMode.Background;
+                //_startBorderBrush = windowMode.BorderBrush;
+
+                //windowMode.Background = _highlightedBackground;
+                //windowMode.BorderBrush = _highlightedBackground;
+
                 WindowStyle = WindowStyle.SingleBorderWindow;
                 ResizeMode = ResizeMode.CanResize;
 
